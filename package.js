f679a479bff872d2ad18ed4edc45379bfa552486
adam wong <adamwong246@gmail.com>
2015-04-06 19:38:48 -0700

    update package name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "yogiben:admin",
+  name: "chroma:admin",
   summary: "A complete admin dashboard solution",
   version: "1.1.0",
   git: "https://github.com/yogiben/meteor-admin"
